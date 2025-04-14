2461153174f2ce995d53daea80c243abb28ff80c
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2025-04-14 19:29:18 -0300

    fix: added useState import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { Server } from "lucide-react"
